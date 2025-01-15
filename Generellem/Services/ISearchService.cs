10d5fbc782a77b9b7856e6bd457c88daf060c8dd
Joe Mayo <6180567+JoeMayo@users.noreply.github.com>
2025-01-14 20:36:08 -0800

    Added support for deleting users. (#190)

--- Generellem/Services/ISearchService.cs
+++ Generellem/Services/ISearchService.cs
@@ -5,6 +5,7 @@ namespace Generellem.Services;
 public interface ISearchService
 {
     Task CreateIndexAsync(CancellationToken cancelToken);
+    Task DeleteAllAsync(int companyId, CancellationToken cancellationToken);
     Task DeleteDocumentReferencesAsync(List<string> documentReferencesToDelete, CancellationToken cancellationToken);
     Task<List<TextChunk>> GetDocumentReferenceAsync(string documentReference, CancellationToken cancellationToken);
     Task<List<TextChunk>> GetDocumentReferencesAsync(string sourceReference, CancellationToken cancellationToken);
